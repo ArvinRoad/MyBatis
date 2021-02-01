d4540bcb6f60ea65bb1a2b7470fb4100f05ece93
ArvinRoad <53329456+ArvinRoad@users.noreply.github.com>
2021-02-01 21:36:27 +0800

    确保SQL资源可以被正常关闭

--- Mybatis-01/src/test/java/com/Demo/dao/UserDaoTest.java
+++ Mybatis-01/src/test/java/com/Demo/dao/UserDaoTest.java
@@ -14,23 +14,29 @@ public class UserDaoTest {
         //获取SqlSession
         SqlSession sqlSession = MybatisUtils.getSqlSession();
 
-        //方式一：getMapper 执行SQL
-        UserDao userDao = sqlSession.getMapper(UserDao.class);
-        List<User> userList = userDao.getUserList();
+        //手动关闭(确保所有数据库资源都能被正常的关闭)
+        try {
+            //方式一：getMapper 执行SQL
+            UserDao userDao = sqlSession.getMapper(UserDao.class);
+            List<User> userList = userDao.getUserList();
 
-        //方式二：了解就可以（已经过时了）
-        List<User> UserList02 = sqlSession.selectList("com.Demo.dao.UserDao.getUserList");
+            //方式二：了解就可以（已经过时了）
+            //List<User> UserList02 = sqlSession.selectList("com.Demo.dao.UserDao.getUserList");
 
-        for (User user : userList) {
-            System.out.println(user);
+            for (User user : userList) {
+                System.out.println(user);
+            }
+
+            //for (User user : UserList02) {
+            //System.out.println(user);
+           // }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            //关闭SqlSession
+            sqlSession.close();
         }
 
-        for (User user : UserList02) {
-            System.out.println(user);
-        }
-
-        //关闭SqlSession
-        sqlSession.close();
     }
 
 }
